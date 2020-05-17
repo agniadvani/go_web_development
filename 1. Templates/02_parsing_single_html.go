b02070907faef81f76687e04f514c42ec2d776e0
agni-advani <advani.agni.agni@gmail.com>
2020-05-17 22:26:12 +0530

    templates

---
+++ 1. Templates/02_parsing_single_html.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("boilet.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nf.Close()
+
+	err = tpl.Execute(nf, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}

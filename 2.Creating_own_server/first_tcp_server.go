7a4bad206dc9e9e8106c746881521f80bd771e39
agni-advani <advani.agni.agni@gmail.com>
2020-05-20 02:11:44 +0530

    tcp servers

---
+++ 2.Creating_own_server/first_tcp_server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Println(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHello from tcp server\n")
+		fmt.Fprintln(conn, "writing on :8080")
+		fmt.Fprintf(conn, "%v", "well i hope")
+
+		conn.Close()
+
+	}
+}

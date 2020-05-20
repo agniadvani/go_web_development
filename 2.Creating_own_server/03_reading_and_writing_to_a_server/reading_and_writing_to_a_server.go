d7420cd9978be8a4495f9f0f24b4dd1fe9628791
agni-advani <advani.agni.agni@gmail.com>
2020-05-20 16:23:50 +0530

    tcp server

---
+++ 2.Creating_own_server/03_reading_and_writing_to_a_server/reading_and_writing_to_a_server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintln(conn, "I Heard you say", ln)
+	}
+
+}

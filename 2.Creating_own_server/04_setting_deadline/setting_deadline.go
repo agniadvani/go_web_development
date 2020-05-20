376b59a0c103c5a0c8cbd2a87a0bee04f1b209c6
agni-advani <advani.agni.agni@gmail.com>
2020-05-20 16:37:37 +0530

    tcp templates

---
+++ 2.Creating_own_server/04_setting_deadline/setting_deadline.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("connection timeout")
+	}
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintln(conn, "I Heard you say", ln)
+	}
+	//breaks out of loop after 10 secs
+	conn.Close()
+	fmt.Println("****CODE PRINTS****")
+
+}

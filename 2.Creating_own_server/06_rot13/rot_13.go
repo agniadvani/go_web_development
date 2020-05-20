f14213978cd6e0ef54449bfcc9810aebe0c483db
agni-advani <advani.agni.agni@gmail.com>
2020-05-20 20:51:05 +0530

    tcp server

---
+++ 2.Creating_own_server/06_rot13/rot_13.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handle(conn)
+	}
+}
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		ls := strings.ToLower(ln)
+		bs := []byte(ls)
+		bsr := rot13(bs)
+		rs := string(bsr)
+		fmt.Fprintln(conn, ln, "-", rs)
+	}
+}
+
+func rot13(bs []byte) []byte {
+	r13 := make([]byte, len(bs))
+	for i, v := range bs {
+		//ascii 97-122
+		if v <= 109 {
+			r13[i] = bs[i] + 13
+		} else {
+			r13[i] = bs[i] - 13
+		}
+
+	}
+	return r13
+}

bf634e884b57dcb0f9d5b5c9d3dd39b4637e989b
agniadvani <advani.agni.agni@gmail.com>
2020-06-06 23:32:16 +0530

    Relational Databases

---
+++ 8.Relational_DB/03.CRUD/04.Delete_Drop/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "user:password@tcp(Localhost:5555)/dbname?charset=utf8")
+	check(err)
+	defer db.Close()
+	err = db.Ping()
+	check(err)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/insert", insert)
+	http.HandleFunc("/read", read)
+	http.HandleFunc("/update", update)
+	http.HandleFunc("/delete", del)
+	http.HandleFunc("/drop", drop)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Database successfully connected.")
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare(`CREATE TABLE customer (name VARCHAR(20));`)
+	check(err)
+
+	rows, err := stmt.Exec()
+	check(err)
+
+	n, err := rows.RowsAffected()
+	check(err)
+
+	fmt.Fprintln(w, "Created Table customer", n)
+}
+
+func insert(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare(`INSERT INTO customer VALUES("James");`)
+	check(err)
+
+	rows, err := stmt.Exec()
+	check(err)
+
+	n, err := rows.RowsAffected()
+	check(err)
+
+	fmt.Fprintln(w, "Inserted Records = ", n)
+}
+
+func read(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query(`SELECT * FROM customer`)
+	check(err)
+	var name string
+	for rows.Next() {
+		err = rows.Scan(&name)
+		check(err)
+	}
+	fmt.Fprintln(w, "Records:", name)
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare(`UPDATE customer SET name = "Jimmy" WHERE name = "James";`)
+	check(err)
+
+	rows, err := stmt.Exec()
+	check(err)
+
+	n, err := rows.RowsAffected()
+	check(err)
+
+	fmt.Fprintln(w, "Records Updated:", n)
+}
+
+func del(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare(`DELETE FROM customer WHERE name = "Jimmy";`)
+	check(err)
+	rows, err := stmt.Exec()
+	check(err)
+	n, err := rows.RowsAffected()
+	check(err)
+	fmt.Fprintln(w, "Records Deleted:", n)
+}
+
+func drop(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare(`DROP TABLE customer;`)
+	check(err)
+	_, err = stmt.Exec()
+	check(err)
+	fmt.Fprintln(w, "Table Customer Deleted")
+}
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
